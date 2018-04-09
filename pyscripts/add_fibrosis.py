e578b290c54b9a3ec7c860e079039a0179375795
Chicken-Bones <mehvids@gmail.com>
2018-04-09 21:16:46 +1000

    Add hdf5 -> vtu converter Refactor fibrosis to numpy

--- pyscripts/add_fibrosis.py
+++ pyscripts/add_fibrosis.py
@@ -1,20 +1,26 @@
+from sys import argv
+from os import path
 import vtk
-import sys
+import numpy as np
+import vtk.util.numpy_support as vtknp
 
+#add_fibrosis.py mesh.vtu fibrosis.txt output.vtu
+
+print('reading: ' + argv[1])
 reader = vtk.vtkXMLUnstructuredGridReader()
-reader.SetFileName(sys.argv[1])
+reader.SetFileName(argv[1])
 reader.Update()
 ugrid = reader.GetOutput()
 
-fibrosis = vtk.vtkFloatArray()
+print('adding: ' + argv[2])
+arr = np.genfromtxt(argv[2], skip_header=1)
+fibrosis = vtknp.numpy_to_vtk(arr, array_type=vtk.VTK_FLOAT)
 fibrosis.SetName('fibrosis')
-with open(sys.argv[2]) as f:
-    for line in f.readlines()[1:]:
-        fibrosis.InsertNextValue(float(line))
-
 ugrid.GetCellData().AddArray(fibrosis)
 
+out_path = argv[3] if len(argv) > 3 else path.splitext(argv[2])[0] + ".vtu"
+print('writing: ' + out_path)
 writer = vtk.vtkXMLUnstructuredGridWriter()
-writer.SetFileName(sys.argv[3])
+writer.SetFileName(out_path)
 writer.SetInputData(ugrid)
 writer.Write()
---
+++ pyscripts/add_hdf5.py
@@ -0,0 +1,47 @@
+from sys import argv
+from os import path
+import vtk
+import h5py
+import numpy as np
+import vtk.util.numpy_support as vtknp
+
+#add_hdf5.py mesh.vtu fibrosis.txt output.vtu
+
+print('reading: ' + argv[1])
+reader = vtk.vtkXMLUnstructuredGridReader()
+reader.SetFileName(argv[1])
+reader.Update()
+ugrid = reader.GetOutput()
+pdata = ugrid.GetPointData()
+num_points = pdata.GetNumberOfTuples()
+
+perm_path = argv[4] if len(argv) > 4 else path.join(path.dirname(argv[2]), "permutation.txt")
+print ('permuting: ' + perm_path)
+if path.isfile(perm_path):
+    perm = np.genfromtxt(perm_path, skip_header=1, dtype=np.int32)[:, 1]
+else:
+    perm = None
+
+print('reading: ' + argv[2])
+h5file = h5py.File(argv[2], 'r')
+for key in h5file.keys():
+    dataset = h5file[key]
+    if len(dataset.shape) != 2 or dataset.shape[1] != num_points:
+        print(dataset.name + ' has wrong shape: ' + dataset.shape)
+        continue
+
+    for i in range(0, dataset.shape[0]):
+        subset = dataset[i, :]
+        if perm is not None:
+            subset = subset[perm]
+
+        arr = vtknp.numpy_to_vtk(subset, deep=1, array_type=vtk.VTK_FLOAT)
+        arr.SetName(dataset.name + '_' + str(i))
+        pdata.AddArray(arr)
+
+out_path = argv[3] if len(argv) > 3 else path.splitext(argv[2])[0] + ".vtu"
+print('writing: ' + out_path)
+writer = vtk.vtkXMLUnstructuredGridWriter()
+writer.SetFileName(out_path)
+writer.SetInputData(ugrid)
+writer.Write()
