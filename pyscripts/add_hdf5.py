2b8e5eefc02e328871f1ed171448f235ee6dd848
Chicken-Bones <mehvids@gmail.com>
2018-04-12 14:51:28 +1000

    Improve hdf5 -> vtu converter More flexible array dimensions (now supports results.h5) Get datasets in order (with leading 0 padding)

--- pyscripts/add_hdf5.py
+++ pyscripts/add_hdf5.py
@@ -1,5 +1,6 @@
 from sys import argv
 from os import path
+import math
 import vtk
 import h5py
 import numpy as np
@@ -26,18 +27,22 @@ print('reading: ' + argv[2])
 h5file = h5py.File(argv[2], 'r')
 for key in h5file.keys():
     dataset = h5file[key]
-    if len(dataset.shape) != 2 or dataset.shape[1] != num_points:
-        print(dataset.name + ' has wrong shape: ' + dataset.shape)
+
+    if len(dataset.shape) < 2 or np.prod(dataset.shape[1:]) != num_points:
+        print(dataset.name + ' has wrong shape: ' + str(dataset.shape))
         continue
 
+    print ('converting: ' + dataset.name)
+    w = math.floor(math.log10(dataset.shape[0]))+1
     for i in range(0, dataset.shape[0]):
-        subset = dataset[i, :]
+        subset = np.squeeze(dataset[i, ...])
         if perm is not None:
             subset = subset[perm]
 
         arr = vtknp.numpy_to_vtk(subset, deep=1, array_type=vtk.VTK_FLOAT)
-        arr.SetName(dataset.name + '_' + str(i))
+        arr.SetName(dataset.name + '_' + str(i).zfill(w))
         pdata.AddArray(arr)
+        print('added: ' + arr.GetName())
 
 out_path = argv[3] if len(argv) > 3 else path.splitext(argv[2])[0] + ".vtu"
 print('writing: ' + out_path)
