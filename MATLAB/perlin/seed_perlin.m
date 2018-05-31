452c782fc24136c726ef668b52832cb81c64cf81
Chicken-Bones <mehvids@gmail.com>
2018-05-31 10:10:49 +1000

    Add doc to seed_perlin, should recognise as matlab file

--- MATLAB/perlin/seed_perlin.m
+++ MATLAB/perlin/seed_perlin.m
@@ -1,3 +1,5 @@
-function P = seed_perlin()
+function [P] = seed_perlin()
+%SEED_PERLIN Perlin Noise Seed Generator
+%   SEED_PERLIN() Generates a permutation table 0-255 for x,y,z
     P = [randperm(256)' randperm(256)' randperm(256)']-1;
 end
